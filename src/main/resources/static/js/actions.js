57f8eadb922e94f7d065d0c461f898d10535779d
Lisakarolina <w.hello@web.de>
2024-03-25 21:00:50 +0100

    Add bookmark logic for template

--- src/main/resources/static/js/actions.js
+++ src/main/resources/static/js/actions.js
@@ -4,3 +4,21 @@ function clickRespectiveCheckbox(elem) {
     // toggle checkbox
     cb.checked = cb.checked === true ? false : true;
 }
+
+function addToFavorites(elem) {
+
+    const star = elem.closest(".bookmark").firstElementChild;
+    const hint = elem.closest(".bookmark").lastElementChild;
+    console.log("found my star: ", star);
+    if(star.classList.contains("marked")) {
+        star.classList.remove("marked");
+        star.classList.add("free");
+        hint.classList.remove("marked");
+        hint.classList.add("free");
+    } else {
+        star.classList.remove("free");
+        star.classList.add("marked");
+        hint.classList.remove("free");
+        hint.classList.add("marked");
+    }
+}
